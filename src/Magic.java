51f17a006862f5a98b07344278e7bf222fd2f05e
Unknown <christian.hayes.2019@student.lhschools.org>
2018-09-11 14:11:09 -0400

    Began declaring and initializing variables

--- src/Magic.java
+++ src/Magic.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Magic {
     public static void main(String [] args){
+        //declare
+        Scanner keyboard;
+        int testing;
+        String question;
+
+
+
+       //intialization phase
+       keyboard = new Scanner(System.in)
+
+
+
 
 
 
@@ -7,3 +21,13 @@ public class Magic {
 
     }
 }
+/*
+10 different choices - 5 positve 5 negative
+
+use the input from the user and a number they give you between 1 and 100
+0-10
+11-20
+21-30
+31-40
+
+ */
